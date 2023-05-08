c978fc891cf4e846318644ebaa6300b1678cf95e
Erick Quintao <erickquintao@outlook.com>
2023-05-08 19:39:56 -0300

    completo loop do carrosel,produtos mais vendidos,depoimentos, caracteristicas da empresa e sobre nos , so falta conserta o botao doido

--- Proj-2.3-Site-Mercava/js/script.js
+++ Proj-2.3-Site-Mercava/js/script.js
@@ -1,18 +1,31 @@
-//  slides automaticos
-let myIndex = 0;
+var myIndex = 0;
 carousel();
 
 function carousel() {
-  let i;
-  let x = document.getElementsByClassName("mySlides");
+  var i;
+  var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 2000);    
 }
-// produtos carrosel
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
 
